8b6887b00ea399943aafea5f8e88f2a1b623113e
Chris Henry <cdhenry@gmail.com>
2018-04-22 15:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ class Route{
   //During off peak hours, a driver travels three blocks in a minute,
   //while during peak hours a driver travels an estimated two blocks in a minute.
   estimatedTime(peak = false) {
-    if (peak){
-      return (this.blocksTravelled() / 3);
-    }else{
-      return (this.blocksTravelled() / 2);
-    }
+    (peak) ? return (this.blocksTravelled() / 3) : return (this.blocksTravelled() / 2);
+    // if (peak){
+    //   return (this.blocksTravelled() / 3);
+    // }else{
+    //   return (this.blocksTravelled() / 2);
+    // }
   }
 }
