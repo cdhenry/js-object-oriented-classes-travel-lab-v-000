f6ec87073837d4bdb2f27fda52b52e0aa7b53389
Chris Henry <cdhenry@gmail.com>
2018-04-22 15:22:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ class Driver{
 }
 
 class Route{
-  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-
   constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
